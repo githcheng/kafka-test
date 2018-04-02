ea9a400cc3605fc6e68da452f94f3d82df3c5a52
chengguojiang <chengguojiang2007@163.com>
2018-04-02 21:00:01 +0800

    init

---
+++ src/main/java/com/hts/ProduceConnection.java
@@ -0,0 +1,12 @@
+package com.hts;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProduceConnection
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
